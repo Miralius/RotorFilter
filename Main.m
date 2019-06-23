9ca22bcd7f05ccb8f9e52bf971fd8196a71026f0
Miralius <f-miralius@yandex.ru>
2019-06-23 14:33:45 +0400

    Внесены изменения в func.m (входную функцию)

--- Main.m
+++ Main.m
@@ -2,13 +2,14 @@ a=2;
 b=2;
 c=2;
 d=2;
-sigma=0.5;
+const=1;
 wavelength=0.00065;
 z=10000;
 n=2000;
 m=2000;
+mm=1;
 % ----------------------------------------------------------
-[input, x, y, h1, h2] = func(a, b, n, sigma);
+[input, x, y, h1, h2] = func(a, b, n, const, mm);
 % --plots of input function--
 figure
 image(abs(input),'CDataMapping','scaled');
@@ -16,10 +17,9 @@ colormap(gray(255))
 figure
 image(angle(input),'CDataMapping','scaled');
 colormap(gray(255))
-display('Lalala!')
 % ----------------------------------------------------------
 tic
-[output] = ft(c, d, m, wavelength, z, h1, h2, input, x, y);
+[output] = ft(c, d, m, h1, h2, input, x, y);
 t=toc;
 % --plots of output function--
 figure
