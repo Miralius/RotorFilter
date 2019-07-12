db5f3fa0886e0f9ede58710a48976e8d12d8e870
Miralius <f-miralius@yandex.ru>
2019-07-12 23:43:31 +0400

    сделана вихревая линза

--- Main.m
+++ Main.m
@@ -4,12 +4,12 @@ c=5;
 d=5;
 const=1;
 wavelength=0.00065;
-z=10000;
-n=1000;
-m=1000;
-mm=6;
+z=5000;
+n=2000;
+m=5000;
+mm=2;
 % ----------------------------------------------------------
-[input, x, y, h1, h2] = func(a, b, n, const, mm);
+[input, x, y, h1, h2] = func(a, b, n, const, mm, z, wavelength);
 % --plots of input function--
 figure
 image(abs(input),'CDataMapping','scaled');
@@ -18,13 +18,13 @@ figure
 image(angle(input),'CDataMapping','scaled');
 colormap(gray(255))
 % ----------------------------------------------------------
-% tic
-% [output] = ft(c, d, m, h1, h2, input, x, y, z, wavelength);
-% t=toc;
+tic
+[output] = ft(c, d, m, h1, h2, input, x, y, z, wavelength);
+t=toc;
 % --plots of output function--
-% figure
-% image(abs(output),'CDataMapping','scaled');
-% colormap(gray(255))
-% figure
-% image(angle(output),'CDataMapping','scaled');
-% colormap(gray(255))
+figure
+image(abs(output),'CDataMapping','scaled');
+colormap(gray(255))
+figure
+image(angle(output),'CDataMapping','scaled');
+colormap(gray(255))
--- func.m
+++ func.m
@@ -1,10 +1,13 @@
-function [input, x, y, h1, h2] = func(a, b, n, const, mm)
+function [input, x, y, h1, h2] = func(a, b, n, const, mm, z, wavelength)
 h1=2*a/n;
 x=-a:h1:a-h1;
 h2=2*b/n;
 y=-b:h2:b-h2;
-gxy=zeros(n, n);
-gxy(1:n,1:n)=const;
+%gxy=zeros(n, n);
+%gxy(1:n,1:n)=const;
+gxy1=exp(-1i*pi*(x.*x)/(wavelength*z));
+gxy2=exp(-1i*pi*(y.*y)/(wavelength*z));
+gxy=gxy1.'*gxy2;
 %input1=(exp(-(x.*x))).';
 %input2=(exp(-(y.*y)));
 %gxy=(input1*input2)/(2*const*const);
